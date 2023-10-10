f350927ac4b35e2f4ee9bcb56d8c47084df6c71a
pluto <2064623160@qq.com>
2023-10-10 16:56:12 +0800

    feat:热门推荐的数据获取

--- src/pages/hot/hot.vue
+++ src/pages/hot/hot.vue
@@ -1,5 +1,8 @@
 // /src/pages/hot/hot.vue
 <script setup lang="ts">
+import { getHotRecommendAPI } from '@/services/hot'
+import { onLoad } from '@dcloudio/uni-app'
+
 // 热门推荐页 标题和url
 const hotMap = [
   { type: '1', title: '特惠推荐', url: '/hot/preference' },
@@ -16,6 +19,17 @@ const query = defineProps<{
 const curUrlMap = hotMap.find((v) => v.type == query.type)
 //动态设置标题
 uni.setNavigationBarTitle({ title: curUrlMap!.title })
+
+// 获取热门推荐数据
+const getHotRecommendData = async () => {
+  const res = await getHotRecommendAPI(curUrlMap!.url)
+  console.log(res)
+}
+
+// 页面加载
+onLoad(() => {
+  getHotRecommendData()
+})
 </script>
 
 <template>
